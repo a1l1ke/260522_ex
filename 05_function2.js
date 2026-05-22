241db83fbf52a59a141e844fa46446122d104f21
a1l1ke <onetenthplease@gmail.com>
2026-05-22 12:19:00 +0900

    feat: 화살표 함수 및 this 바인딩 예제 추가

--- 05_function2.js
+++ 05_function2.js
@@ -19,7 +19,27 @@ f(function () {
 a(); // 선언 전에도
 // html? script -> 언제 해석될지 모름. html에 button, onclick 속성에 js 함수. html이 먼저 등장해서 위치를 잡은다음 뒤에 script. (var, function) / hoist (감아올리기) -> let, const. function? -> let. const. 표현식 = 익명함수.
 function a() {
+  console.log(this);
   console.log("호이호이호이스팅");
 }
 
 // 화살표 함수 (유사 lambda)
+// 하스켈, 리스프 - (스칼라...) - 클로저 - ocaml
+(function () {});
+// (입력) => 출력
+// (입력) => {과정 return 출력}
+const af = () => 0;
+af();
+console.log(af());
+const af2 = (a) => console.log(`실행 결과 : ${a}`);
+console.log(af2(1));
+// a => console.log(`실행 결과 : ${a}`);
+// 매개변수가 1개 있을 때는 괄호 생략 가능
+const af3 = (a) => {
+  // 블록을 넣어서 최종적으로 과정을 거쳐 return
+  console.log(this);
+  a++;
+  a *= 2;
+  return a;
+};
+console.log(af3(100));
